ed0d8c85beeb2b84fcf9d6e756c7cc3db3a69012
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-10-20 02:47:36 +1100

    path lib removed

--- src/app.js
+++ src/app.js
@@ -30,10 +30,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // http://www.tutorialsteacher.com/nodejs/serving-static-files-in-nodejs   ?
 //Static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
 
 //Assign Route definitions to the request
 app.use("/", appRoutes);
